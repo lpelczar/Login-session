086e35728f7c94089913d1aec9be53b3c4ef0e04
lpelczar <lukepelczar@gmail.com>
2018-04-14 13:16:36 +0200

    Add creating database script and sample database

---
+++ src/main/resources/createDatabase.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+  userID INTEGER PRIMARY KEY,
+  login TEXT,
+  password TEXT
+);
+
+INSERT INTO users VALUES (1, "johndoe", "doe");
+INSERT INTO users VALUES (2, "pablo", "escobar");
