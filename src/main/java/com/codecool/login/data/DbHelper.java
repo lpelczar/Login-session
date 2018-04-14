f1a7d398e9c1eff0fc343052c970127cc07c2ff1
lpelczar <lukepelczar@gmail.com>
2018-04-14 12:58:29 +0200

    Add db helper class

---
+++ src/main/java/com/codecool/login/data/DbHelper.java
@@ -0,0 +1,58 @@
+package com.codecool.login.data;
+
+import org.sqlite.SQLiteConfig;
+
+import java.sql.*;
+
+public class DbHelper {
+
+    private static final String DB_URL = "jdbc:sqlite:src/main/resources/users.db";
+    private static final String DRIVER = "org.sqlite.JDBC";
+    private Connection connection;
+
+    private void openConnection() {
+
+        try {
+            SQLiteConfig config = new SQLiteConfig();
+            config.enforceForeignKeys(true);
+            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(DB_URL, config.toProperties());
+        } catch ( Exception e ) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+    }
+
+    protected void closeConnection() {
+
+        if (connection != null)
+            try {
+                connection.close();
+            } catch (SQLException e) { /*ignored*/ }
+    }
+
+    protected PreparedStatement getPreparedStatement(String sqlStatement) throws SQLException {
+        openConnection();
+        return connection.prepareStatement(sqlStatement);
+    }
+
+    protected ResultSet query(PreparedStatement statement) throws SQLException {
+        return statement.executeQuery();
+    }
+
+    protected boolean update(PreparedStatement statement) {
+
+        try {
+            openConnection();
+            connection.setAutoCommit(false);
+            statement.executeUpdate();
+            connection.commit();
+            return true;
+        } catch (SQLException e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        } finally {
+            closeConnection();
+        }
+        return false;
+    }
+}
