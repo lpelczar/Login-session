78d6dbefaec9058323857412e6a5903365ae01c2
lpelczar <lukepelczar@gmail.com>
2018-04-14 12:57:00 +0200

    Add user pojo class

---
+++ src/main/java/com/codecool/login/models/User.java
@@ -0,0 +1,38 @@
+package com.codecool.login.models;
+
+public class User {
+
+    private int userId;
+    private String login;
+    private String password;
+
+    public User(int userId, String login, String password) {
+        this.userId = userId;
+        this.login = login;
+        this.password = password;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
